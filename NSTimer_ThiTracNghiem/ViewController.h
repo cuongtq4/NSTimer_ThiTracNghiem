2352e5cfc9e96e173f4fd4518893217145b908f7
cuongtq4 <thieuquangcuong1@gmail.com>
2015-03-29 12:04:55 +0700

    cuongtq4
    first commit!

--- NSTimer_ThiTracNghiem/ViewController.h
+++ NSTimer_ThiTracNghiem/ViewController.h
@@ -8,8 +8,28 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    IBOutlet UILabel *CauHoiSo;
+    IBOutlet UILabel *NoiDungCauHoi;
+    IBOutlet UILabel *DapAnA;
+    IBOutlet UILabel *DapAnB;
+    IBOutlet UILabel *DapAnC;
+    IBOutlet UISegmentedControl *CauTraLoi;
+    IBOutlet UILabel *KetQua;
+    IBOutlet UIButton *CauTiepTheo;
+    
+    NSTimer *ThoiGian;
+    int Diem;
+    int CauHoiDangLam;
+    int SoCauTraLoiSai;
+    NSArray *CacCauHoi;
+    NSArray *CacCauTraLoi;
+    NSArray *DapAn;
+}
 
+- (IBAction)CauTraLoi:(id)sender;
+- (IBAction)CauTiepTheo:(id)sender;
 
 @end
 
+ 
--- NSTimer_ThiTracNghiem/ViewController.m
+++ NSTimer_ThiTracNghiem/ViewController.m
@@ -17,6 +17,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    Diem = 0;
+    CauHoiDangLam = 0;
+    SoCauTraLoiSai = 0;
+    CacCauHoi = [NSArray arrayWithObjects:
+                 @"Question 1",
+                 @"Question 2",
+                 @"Question 3",
+                 @"Question 4",
+                 @"Question 5",nil];
+    CacCauTraLoi = [NSArray arrayWithObjects:
+                    @"dap an sai-dap an dung-dap an sai",
+                    @"dap an dung-dap an sai-dap an sai",
+                    @"dap an dung-dap an sai-dap an sai",
+                    @"dap an sai-dap an sai-dap an dung",
+                    @"dap an sai-dap an sai-dap an dung",nil];
+    DapAn = [NSArray arrayWithObjects:
+             @"1",
+             @"0",
+             @"0",
+             @"2",
+             @"2",nil];
+    
+    ThoiGian = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(ThucThi) userInfo:nil repeats:YES];
+    [CauHoiSo setText:[NSString stringWithFormat:@"Câu hỏi số: %i",CauHoiDangLam + 1]];
+    [NoiDungCauHoi setText:[CacCauHoi objectAtIndex:CauHoiDangLam]];
+    NSArray *CacDapAn = [[CacCauTraLoi objectAtIndex:CauHoiDangLam] componentsSeparatedByString:@"-"];
+    [DapAnA setText:[NSString stringWithFormat:@"Dap An A: %@", [CacDapAn objectAtIndex:0]]];
+    [DapAnB setText:[NSString stringWithFormat:@"Dap An B: %@", [CacDapAn objectAtIndex:1]]];
+    [DapAnC setText:[NSString stringWithFormat:@"Dap An C: %@", [CacDapAn objectAtIndex:2]]];
+    
+}
+
+- (void)ThucThi {
+    static int i = 0;
+    NSLog(@"%i", i);
+    i++;
+    if(i == 30) {
+        [ThoiGian invalidate];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +63,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)CauTraLoi:(id)sender {
+    NSInteger DapAnTuNguoiDung = [CauTraLoi selectedSegmentIndex];
+    NSInteger DapAnChinhThuc = [[DapAn objectAtIndex:CauHoiDangLam] intValue];
+    if (DapAnTuNguoiDung == DapAnChinhThuc) {
+        //Log
+        Diem = Diem + 1;
+        [KetQua setText:@"Chinh Xac"];
+    } else {
+        SoCauTraLoiSai += 1;
+        [KetQua setText:@"Sai Cmnr!"];
+    }
+}
+- (IBAction)CauTiepTheo:(id)sender {
+    if (CauHoiDangLam == 3) {
+        [CauTiepTheo setTitle:@"Ket Qua" forState:UIControlStateNormal];
+    }
+    if (CauHoiDangLam == 4) {
+        //Xuat diem
+        NSLog(@"Diem cua ban la: %i", Diem);
+        NSLog(@"Ban tra loi dung %i/5 cau hoi", 5 - SoCauTraLoiSai);
+        [CauTiepTheo setEnabled:NO];
+        [NoiDungCauHoi setText:[NSString stringWithFormat:@"Ban da tra loi dung %i cau tren tong so 5 cau.", 5 - SoCauTraLoiSai]];
+        
+    } else {
+        [KetQua setText:@""];
+        [CauTraLoi setSelectedSegmentIndex:-1];
+        CauHoiDangLam = CauHoiDangLam + 1;
+        [CauHoiSo setText:[NSString stringWithFormat:@"Câu hỏi số: %i",CauHoiDangLam + 1]];
+        [NoiDungCauHoi setText:[CacCauHoi objectAtIndex:CauHoiDangLam]];
+        NSArray *CacDapAn = [[CacCauTraLoi objectAtIndex:CauHoiDangLam] componentsSeparatedByString:@"-"];
+        [DapAnA setText:[NSString stringWithFormat:@"Dap An A: %@", [CacDapAn objectAtIndex:0]]];
+        [DapAnB setText:[NSString stringWithFormat:@"Dap An B: %@", [CacDapAn objectAtIndex:1]]];
+        [DapAnC setText:[NSString stringWithFormat:@"Dap An C: %@", [CacDapAn objectAtIndex:2]]];
+    }
+}
 @end
